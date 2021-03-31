413e43093b16f459f0059ae2266dccadc0c747fc
nerohiro <49454170+nerohiro@users.noreply.github.com>
2021-03-31 18:39:04 -0500

    tweak(scenes): added logging for scenes for those naughty people

--- server.lua
+++ server.lua
@@ -13,9 +13,18 @@ RegisterNetEvent('nh-scenes:add', function(coords, message, color, distance)
         coords = coords
     })
     TriggerClientEvent('nh-scenes:send', -1, scenes)
+    TriggerEvent('nh-scenes:log', source, message, coords)
 end)
 
 RegisterNetEvent('nh-scenes:delete', function(key)
     table.remove(scenes, key)
     TriggerClientEvent('nh-scenes:send', -1, scenes)
+end)
+
+
+RegisterNetEvent('nh-scenes:log', function(id, text, coords)
+    local f, err = io.open('sceneLogging.txt', 'a')
+    if not f then return print(err) end
+    f:write('Player: ['..id..'] Placed Scene: ['..text..'] At Coords = '..coords..'\n')
+    f:close()
 end)
